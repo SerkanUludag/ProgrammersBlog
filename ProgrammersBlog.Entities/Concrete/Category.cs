e3b1c3c6e40f792472d010bf96db06153136bcd7
SerkanUludag <hsuluda1@gmail.com>
2021-10-29 10:52:23 +0300

    Add project files.

---
+++ ProgrammersBlog.Entities/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using ProgrammersBlog.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entity.Concrete
+{
+    public class Category : EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; }
+
+    }
+}
