6f09917fb42d4615fad1a1f81fd770e5be83d050
SerkanUludag <hsuluda1@gmail.com>
2022-01-16 15:50:00 +0300

    identity user system

---
+++ ProgrammersBlog.Entities/DTOs/UserPasswordChangeDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entity.DTOs
+{
+    public class UserPasswordChangeDto
+    {
+        [DisplayName("Current Password")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [MaxLength(30, ErrorMessage = "{0} must be less than {1} characters.")]
+        [MinLength(5, ErrorMessage = "{0} must be greater than {1} characters.")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [DisplayName("New Password")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [MaxLength(30, ErrorMessage = "{0} must be less than {1} characters.")]
+        [MinLength(5, ErrorMessage = "{0} must be greater than {1} characters.")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [DisplayName("Repeat New Password")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [MaxLength(30, ErrorMessage = "{0} must be less than {1} characters.")]
+        [MinLength(5, ErrorMessage = "{0} must be greater than {1} characters.")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Passwords not match!")]                    // compare
+        public string RepeatPassword { get; set; }
+    }
+}
